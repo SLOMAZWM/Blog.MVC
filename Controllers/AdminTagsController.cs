cd28080a3981b14894e33cef3e5b9d7b95b1b691
Marcin.Slomka2001 <marcin.slomka2001@gmail.com>
2024-08-19 12:02:02 +0200

    AdminTags created

---
+++ Controllers/AdminTagsController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Blog.MVC.Controllers
+{
+    public class AdminTagsController : Controller
+    {
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+    }
+}
