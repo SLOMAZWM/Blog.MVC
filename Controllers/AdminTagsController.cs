955d1eec26b224101718c6504c94f5929d5b0d72
Marcin.Slomka2001 <marcin.slomka2001@gmail.com>
2024-08-21 10:35:30 +0200

    Add tags to adminController

--- Controllers/AdminTagsController.cs
+++ Controllers/AdminTagsController.cs
@@ -1,13 +1,45 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Blog.MVC.Data;
+using Blog.MVC.Models.Domain;
+using Blog.MVC.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Blog.MVC.Controllers
 {
     public class AdminTagsController : Controller
     {
+        private readonly BloggieDbContext _bloggieDbContext;
+        public AdminTagsController(BloggieDbContext bloggieDbContext)
+        {
+            _bloggieDbContext = bloggieDbContext;
+        }
+
         [HttpGet]
         public IActionResult Add()
         {
             return View();
+        }
+
+        [HttpPost]
+        [ActionName("Add")]
+        public IActionResult Add(AddTagRequest addTagRequest)
+        {
+            // Mapping AddTagRequest to Tag Domain Model
+            _bloggieDbContext.Tags.Add(new Tag
+            {
+                Name = addTagRequest.Name,
+                DisplayName = addTagRequest.DisplayName
+            });
+            _bloggieDbContext.SaveChanges();
+
+            return View("Add");
+        }
+
+        [HttpGet]
+        public IActionResult List()
+        {
+            var tags = _bloggieDbContext.Tags.ToList();
+
+            return View(tags);
         }
     }
 }
